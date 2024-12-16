5efe9e10fee542f4f77f20fe0ae2c19a09d288e0
jhawits <153328440+jhawits@users.noreply.github.com>
2024-12-16 16:19:38 -0600

    Update contact.js

--- functions/api/contact.js
+++ functions/api/contact.js
@@ -25,7 +25,7 @@ async function handleRequest({ request, env }) {
 
   // Send message :)
   // Just remove the comment from whichever one you want
-  await sendDiscordMessage(name, message, env.DISCORD_WEBHOOK_URL);
+  await sendDiscordMessage(name, message, email, env.TEAMS_WEBHOOK_URL);
   // await sendEmailWithSendGrid();
 
   return new Response('Thanks for contacting us! ');
@@ -36,11 +36,7 @@ async function handleRequest({ request, env }) {
 // ---
 // This function will send a Discord message to the supplied webhook URL
 
-env {
-  DISCORD_WEBHOOK_URL="https://weightlessit.webhook.office.com/webhookb2/6f161954-bc31-46fe-b379-c3497e3fa362@f28427fe-7802-4a8a-97ef-3829d92825e6/IncomingWebhook/8467c9aea97f4cebb2e11ca4548d08a9/a9671250-5bf7-4006-abf2-dbe8d1b969ab/V2v129VXIiKA24OFSkiGfo6fivRU23S9lRphEit2AAnA41"
-}
-
-async function sendDiscordMessage(name, message, webhookUrl) {
+async function sendDiscordMessage(name, message, email, webhookUrl) {
   await fetch(webhookUrl, {
     method: 'POST',
     headers: {
