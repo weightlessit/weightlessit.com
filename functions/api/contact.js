c9df4db6a030aff5b62078fb67eaf5df0fecaf42
jhawits <153328440+jhawits@users.noreply.github.com>
2024-12-17 08:55:05 -0600

    Update contact.js

--- functions/api/contact.js
+++ functions/api/contact.js
@@ -46,28 +46,22 @@ async function sendTeamsMessage(name, message, email, webhookUrl) {
       summary: 'summary',
       embeds: [
             {
-                "contentType": "application/vnd.microsoft.card.adaptive",
-                "contentUrl": null,
-                "content": {
-                    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-                    "type": "AdaptiveCard",
-                    "version": "1.2",
-                    "body": [
-                        {
-			    "type": "text",
-			    "text": name
-			},
-			{
-			    "type": "text",
-			    "text": email
-			},
-			{
-                "type": "TextBlock",
-                "text": message
-                        }
-                    ]
-                }
-            }]
+                "contentType":"application/vnd.microsoft.card.adaptive",
+             "contentUrl":null,
+             "content":{
+                "$schema":"http://adaptivecards.io/schemas/adaptive-card.json",
+                "type":"AdaptiveCard",
+                "version":"1.2",
+                "body":[
+                    {
+                    "type": "TextBlock",
+                    "text": "For Samples and Templates, see [https://adaptivecards.io/samples](https://adaptivecards.io/samples)"
+                    }
+                ]
+             }
+          }
+       ]
+    }]
     }),
   });
 };
